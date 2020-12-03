0d520a1d02ec97bb737a2ccc290cebe52214482b
Gabriel-Martinez0 <gabriel.martinez@etu.umontpellier.fr>
2020-12-03 21:10:54 +0100

    pour view

--- controller/routeur.php
+++ controller/routeur.php
@@ -3,7 +3,13 @@
 require_once File::build_path(array("controller","ControllerCompteRendu.php"));
 
 // On recupère l'action passée dans l'URL
-$action = $_GET["action"];
+if (isset($_GET['action']))
+{
+	$action = $_GET["action"];
+}
+else {
+	$action = "readAll";
+}
 // Appel de la méthode statique $action de ControllerVoiture
 ControllerCompteRendu::$action();
 
