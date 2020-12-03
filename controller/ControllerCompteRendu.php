3d3880837b3acc66ba50c13ffd9bfcd37838a18b
Gabriel-Martinez0 <gabriel.martinez@etu.umontpellier.fr>
2020-12-03 20:26:25 +0100

    pour view

--- controller/ControllerCompteRendu.php
+++ controller/ControllerCompteRendu.php
@@ -1,1 +1,15 @@
 <?php
+require_once File::build_path(array("model","ModelCompteRendu.php"));
+
+class ControllerCompteRendu {
+
+	public static function readAll() {
+		//$tab_v = ModelCompteRendu::getAllCompteRendu();     //appel au modèle pour gerer la BD
+		$controller='CompteRendu';
+		$view='list';
+		$pagetitle='Liste des compte rendu';
+		require File::build_path(array("view","view.php"));  //"redirige" vers la vue
+
+	}
+}
+?>
