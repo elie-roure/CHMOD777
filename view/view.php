1fd7191c07ef13cde4829e9d45586c531f37b654
ylow <ylona.fabiani@etu.umontpellier.fr>
2020-12-03 22:35:12 +0100

    nav

--- view/view.php
+++ view/view.php
@@ -3,28 +3,28 @@
 	<head>
 		<meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="./css/cssindex.css">
-		<title><?php echo "titre"; ?></title>
+		<title><?php echo "BioSurfers"; ?></title>
 	</head>
 	<body>
 	<header>
         <nav>
-            <div><a href="?JESAISPASCEQUEJEDOISECRIREICI">Spots</a></div>
-            <div><a href="?JESAISPASCEQUEJEDOISECRIREICI">Comptes Rendus</a></div>
-            <div><a href="?JESAISPASCEQUEJEDOISECRIREICI">A Propos</a></div>
-            <div><a href="?JESAISPASCEQUEJEDOISECRIREICI">Contact</a></div>
+            <div><a href="?action=readAll&controller=spot">Spots</a></div>
+            <div><a href="?action=readAll&controller=compterendu">Comptes Rendus</a></div>
+            <div><a href="?action=readAll&controller=utilisateur">Utilisateurs</a></div>
+            <div><a href="?LIEN CLASSIQUE">A Propos</a></div>
+            <div><a href="?LIEN CLASSIQUE">Contact</a></div>
         </nav>
 	</header>
 
-	<?php
-        $filepath = File::build_path(array("view", $controller, "$view.php"));
-        require $filepath;
-	?>
+     <?php /*
+    $filepath = File::build_path(array("view", $controller, "$view.php"));
+    require $filepath;
+    */?>
 
 	<footer>
-		<p style="border: 1px solid black;text-align:right;padding-right:1em;">
-			Site BioSurfers
-		</p>
+        <p style="border: 1px solid black;text-align:right;padding-right:1em;">
+            Site BioSurfers
+        </p>
 	</footer>
-
 	</body>
 </html>
