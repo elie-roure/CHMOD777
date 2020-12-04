37520a7bbe73a0ea05a55d3ad4dbce642a12336f
ylow <ylona.fabiani@etu.umontpellier.fr>
2020-12-04 05:43:18 +0100

    forms

--- view/view.php
+++ view/view.php
@@ -9,13 +9,13 @@
         </script>
 	</head>
 	<body>
-    <div><a href="?controller=accueil"><img src="./css/img/_logo.png" alt="logo BioSurfers" class="logo"</a></div>
     <div class="connexions">
     <?=(isset($_SESSION['login'])) ? "<div><a href='?action=read&controller=utilisateur&login=".$_SESSION['login']."'>Profil</a></div>" : ""?>
     <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=utilisateur'>Inscription</a></div>" : ""?>
     <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=connect&controller=utilisateur'>Connexion</a></div>" : ""?>
     <?=(isset($_SESSION['login'])) ? "<div><a href='?action=deconnect&controller=utilisateur'>Deconnexion</a></div>" : ""?>
     </div>
+    <div><a href="?controller=accueil"><img src="./css/img/_logo.png" alt="logo BioSurfers" class="logo"</a></div>
 	<header>
         <nav>
             <div><a href="?controller=accueil">Accueil</a></div>
