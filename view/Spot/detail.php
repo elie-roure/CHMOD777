362427db9750d2e18f9b6aa2b111b1c87e0fb431
Gabriel-Martinez0 <gabriel.martinez@etu.umontpellier.fr>
2020-12-04 05:09:56 +0100

    maj

--- view/Spot/detail.php
+++ view/Spot/detail.php
@@ -1,5 +1,47 @@
-<?php
-	htmlspecialchars($s->afficher());
-	echo ' <a href="index.php?action=delete&controller=spot&id_spot=' . rawurlencode($s->getIdSpot()) . '">X' . '</a>' ;
-	echo ' <a href="index.php?action=update&controller=spot&id_spot=' . rawurlencode($s->getIdSpot()) . '">maj ' . '</a>' ;
-?>
+<div>
+	<?php
+
+		echo '    <div class="cadre">';
+		echo '        <div>';
+
+		echo '            <p>';
+		echo '              Compte rendu n° ' . htmlspecialchars($v->getIdCompteRendu());
+		echo '            </p>';
+
+		echo '            <p>';
+		echo '  <a href="index.php?action=delete&id_compteRendu=' . rawurlencode($v->getIdCompteRendu()) . '">X</a>' . ' <a href="index.php?action=update&id_compteRendu=' . rawurlencode($v->getIdCompteRendu()) . '">maj</a>';
+		echo '            </p>';
+
+		echo '              <a href="index.php?action=read&id_compteRendu=' . rawurlencode($v->getIdCompteRendu()). '">En savoir plus</a>';
+
+		echo '</div>';
+		echo '</div>';
+		htmlspecialchars($s->afficher());
+		echo ' <a href="index.php?action=delete&controller=spot&id_spot=' . rawurlencode($s->getIdSpot()) . '">X' . '</a>' ;
+		echo ' <a href="index.php?action=update&controller=spot&id_spot=' . rawurlencode($s->getIdSpot()) . '">maj ' . '</a>' ;
+	?>
+</div>
+
+<div class="surcadrage">
+	<?php
+	foreach ($tab_c as $v){
+
+		echo '    <div class="cadre">';
+		echo '        <div>';
+
+		echo '            <p>';
+		echo '              Compte rendu n° ' . htmlspecialchars($v->getIdCompteRendu());
+		echo '            </p>';
+
+		echo '            <p>';
+		echo '  <a href="index.php?action=delete&id_compteRendu=' . rawurlencode($v->getIdCompteRendu()) . '">X</a>' . ' <a href="index.php?action=update&id_compteRendu=' . rawurlencode($v->getIdCompteRendu()) . '">maj</a>';
+		echo '            </p>';
+
+		echo '              <a href="index.php?action=read&id_compteRendu=' . rawurlencode($v->getIdCompteRendu()). '">En savoir plus</a>';
+
+		echo '</div>';
+		echo '</div>';
+
+	}
+	?>
+</div>
--- view/Spot/list.php
+++ view/Spot/list.php
@@ -6,11 +6,11 @@
 		echo '        <div>';
 
 		echo '            <p>';
-		echo '              Spot d\'id ' . htmlspecialchars($v->getIdspot()) . '  <a href="index.php?action=delete&controller=spot&id_spot=' . rawurlencode($v->getIdspot()) . '">X</a>' . ' <a href="index.php?action=update&controller=spot&id_spot=' . rawurlencode($v->getIdspot()) . '">maj</a>';
+		echo '              Spot n° ' . htmlspecialchars($v->getIdspot()) . '  <a href="index.php?action=delete&controller=spot&id_spot=' . rawurlencode($v->getIdspot()) . '">X</a>' . ' <a href="index.php?action=update&controller=spot&id_spot=' . rawurlencode($v->getIdspot()) . '">maj</a>';
 		echo '            </p>';
 
-		echo '            <p>';
-		echo htmlspecialchars($v->getNom()) . '  <a href="index.php?action=delete&controller=spot&id_spot=' . rawurlencode($v->getIdspot()) . '">X</a>' . ' <a href="index.php?action=update&controller=spot&id_spot=' . rawurlencode($v->getIdspot()) . '">maj</a>';
+		echo '            <p class="nomspot">';
+		echo htmlspecialchars($v->getNom());
 		echo '            </p>';
 
 		echo '              <a href="index.php?action=read&controller=spot&id_spot=' . rawurlencode($v->getIdspot()). '">En savoir plus</a>';
