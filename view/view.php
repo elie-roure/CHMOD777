5cc6c12b7eee246e5c1c3c55f2b106c124eb1168
ylow <ylona.fabiani@etu.umontpellier.fr>
2020-12-04 05:17:50 +0100

    view

--- view/view.php
+++ view/view.php
@@ -10,6 +10,12 @@
 	</head>
 	<body>
     <div><a href="?controller=accueil"><img src="./css/img/_logo.png" alt="logo BioSurfers" class="logo"</a></div>
+    <div class="connexions">
+    <?=(isset($_SESSION['login'])) ? "<div><a href='?action=read&controller=utilisateur&login=".$_SESSION['login']."'>Profil</a></div>" : ""?>
+    <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=utilisateur'>Inscription</a></div>" : ""?>
+    <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=connect&controller=utilisateur'>Connexion</a></div>" : ""?>
+    <?=(isset($_SESSION['login'])) ? "<div><a href='?action=deconnect&controller=utilisateur'>Deconnexion</a></div>" : ""?>
+    </div>
 	<header>
         <nav>
             <div><a href="?controller=accueil">Accueil</a></div>
@@ -25,10 +31,6 @@
                     <?=(isset($_SESSION['login'])) ? "<div><a href='?action=create'>Créateur</a></div>" : ""?>
                 </div>
             </div>
-            <?=(isset($_SESSION['login'])) ? "<div><a href='?action=read&controller=utilisateur&login='".$_SESSION['login'].">Profil</a></div>" : ""?>
-            <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=utilisateur'>Inscription</a></div>" : ""?>
-            <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=connect&controller=utilisateur'>Connexion</a></div>" : ""?>
-            <?=(isset($_SESSION['login'])) ? "<div><a href='?action=deconnect&controller=utilisateur'>Deconnexion</a></div>" : ""?>
             <div><a href="?controller=apropos">A Propos</a></div>
             <div><a href="?controller=contact">Contact</a></div>
         </nav>
