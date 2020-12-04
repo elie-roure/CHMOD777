7d2c1be75f2fb824a9589b5547a02a97aaccec89
ylow <ylona.fabiani@etu.umontpellier.fr>
2020-12-04 06:31:37 +0100

    ct long le css

--- view/view.php
+++ view/view.php
@@ -9,32 +9,38 @@
         </script>
 	</head>
 	<body>
-    <div class="connexions">
-    <?=(isset($_SESSION['login'])) ? "<div><a href='?action=read&controller=utilisateur&login=".$_SESSION['login']."'>Profil</a></div>" : ""?>
-    <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=utilisateur'>Inscription</a></div>" : ""?>
-    <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=connect&controller=utilisateur'>Connexion</a></div>" : ""?>
-    <?=(isset($_SESSION['login'])) ? "<div><a href='?action=deconnect&controller=utilisateur'>Deconnexion</a></div>" : ""?>
+    <div class="tout">
+        <div><a href="?controller=accueil"><img src="./css/img/_logo.png" alt="logo BioSurfers" class="logo"</a></div>
+        <div class="presquetout">
+            <div class="connexions">
+            <?=(isset($_SESSION['login'])) ? "<div><a href='?action=read&controller=utilisateur&login=".$_SESSION['login']."'>Profil</a></div>" : ""?>
+            <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=utilisateur'>Inscription</a></div>" : ""?>
+            <?=(!isset($_SESSION['login'])) ? "<div><a href='?action=connect&controller=utilisateur'>Connexion</a></div>" : ""?>
+            <?=(isset($_SESSION['login'])) ? "<div><a href='?action=deconnect&controller=utilisateur'>Deconnexion</a></div>" : ""?>
+            </div>
+
+            <header>
+                <nav>
+                    <div><a href="?controller=accueil">Accueil</a></div>
+                    <div><a href="#">Spots</a>
+                        <div class="submenu">
+                            <div><a href="?action=readAll&controller=spot">Liste</a></div>
+                            <?=(isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=spot'>Créateur</a></div>" : ""?>
+                        </div>
+                    </div>
+                    <div><a href="#">Comptes Rendus</a>
+                        <div class="submenu">
+                            <div><a href="?action=readAll&controller=CompteRendu">Liste</a></div>
+                            <?=(isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=CompteRendu'>Créateur</a></div>" : ""?>
+                        </div>
+                    </div>
+                    <div><a href="?controller=apropos">A Propos</a></div>
+                    <div><a href="?controller=contact">Contact</a></div>
+                </nav>
+            </header>
+        </div>
     </div>
-    <div><a href="?controller=accueil"><img src="./css/img/_logo.png" alt="logo BioSurfers" class="logo"</a></div>
-	<header>
-        <nav>
-            <div><a href="?controller=accueil">Accueil</a></div>
-            <div><a href="#">Spots</a>
-                <div class="submenu">
-                    <div><a href="?action=readAll&controller=spot">Liste</a></div>
-                    <?=(isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=spot'>Créateur</a></div>" : ""?>
-                </div>
-            </div>
-            <div><a href="#">Comptes Rendus</a>
-                <div class="submenu">
-                    <div><a href="?action=readAll&controller=CompteRendu">Liste</a></div>
-                    <?=(isset($_SESSION['login'])) ? "<div><a href='?action=create&controller=CompteRendu'>Créateur</a></div>" : ""?>
-                </div>
-            </div>
-            <div><a href="?controller=apropos">A Propos</a></div>
-            <div><a href="?controller=contact">Contact</a></div>
-        </nav>
-	</header>
+
 
 
     <main>
