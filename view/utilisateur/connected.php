3f087f50beb5e8f01159a05c4ede170aefe40467
johan-daniel <johan.daniel@etu.umontpellier.fr>
2020-12-04 05:38:33 +0100

    gngngn

--- view/utilisateur/connected.php
+++ view/utilisateur/connected.php
@@ -1,4 +1,3 @@
 <?php
-    echo "<p>Vous êtes connectés en tant que " . $_SESSION['login'] . "</p>";
     require  File::build_path(array('view', 'utilisateur', 'details.php'));
 ?>
--- view/utilisateur/details.php
+++ view/utilisateur/details.php
@@ -1,5 +1,10 @@
 <?php
-htmlspecialchars($user->afficher());
+$user = ModelUtilisateur::getUtilisateurByLogin($_GET['login']);
+echo "<div>htmlspecialchars('Vous êtes connectés en tant que ' . $user->$getLogin())</div>";
+echo "<div>htmlspecialchars($user->getPrenom() . ' ' . $user->getNom())</div>";
+echo "<div>htmlspecialchars($user->getAdresse())</div>";
+echo "<div>htmlspecialchars($user->getMail())</div>";
+echo "<div>htmlspecialchars($user->getTel())</div>";
 
 echo '<br><p> <a href="index.php?action=delete&controller=utilisateur&login=' . rawurlencode($user->getLogin()) . '"> Supprimer </a> </p>';
 echo '<p> <a href="index.php?action=update&controller=utilisateur&login=' . rawurlencode($user->getLogin()) . '"> Modifier </a> </p>';
